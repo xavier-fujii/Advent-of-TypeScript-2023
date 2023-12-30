9cef64923f00617c6540676508d9155b116a5fb8
Xavier Fujii <nothingtoworry7@gmail.com>
2023-12-31 00:40:50 +0800

    fix day 12

---
+++ day15.ts
@@ -0,0 +1,8 @@
+// type BoxToys<T, U extends number> = unknown;
+
+// type test_nutcracker_actual = BoxToys<"nutcracker", 3 | 4>;
+// type test_nutcracker_expected =
+//   | ["nutcracker", "nutcracker", "nutcracker"]
+//   | ["nutcracker", "nutcracker", "nutcracker", "nutcracker"];
+
+// type BuildTuple<TItem extends any, TLength extends number> =
